acb61ef5be5c35b2441da567f86ca4b6f8010972
Wajih-tech <109511329+Wajih-tech@users.noreply.github.com>
2022-07-18 16:26:08 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+//module
+// const a = {
+//     average:(a,b)=>{
+//         console.log((a+b)/2);
+//     }};
+//     module.exports = a;
+//file system
+// const fs =require("fs");
+
+// fs.readFile("./sample.txt" ,"utf-8" ,(err,data)=>{
+//     if(err)
+//     return err;
+//     else
+//     console.log(data);
+// });
+const http = require("http");
+const fs = require("fs");
+
+const PORT = 3000;
+//process.env.PORT
+const HostName = "localhost";
+const home = fs.readFileSync("./index.html","utf-8");
+const notFound = fs.readFileSync("./notFound.html","utf-8");
+const contact = fs.readFileSync("./contact.html","utf-8");
+const about = fs.readFileSync("./about.html","utf-8");
+const chart = fs.readFileSync("./chart.html","utf-8");
+
+ const server = http.createServer((req,res)=>{
+    if (req.url === "/"){
+       return res.end(home);
+    }
+     if (req.url === "/about"){
+       return res.end(about);
+    }
+    if (req.url === "/chart"){
+        return res.end(chart);
+       }   else {
+       return res.end(notFound);
+     }
+});
+
+server.listen(PORT,HostName,()=>{
+    console.log(`server is running on http://${HostName}:${PORT}`);
+});
